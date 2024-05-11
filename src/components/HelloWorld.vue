15d44a076cdb58288246e3077c22c1f79ee811b2
KamiiLiu <k01midy@gmail.com>
2024-05-11 23:10:17 +0800

    feat:增加env環境

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,44 @@
 <template>
-  <div>
-    <button type="button" class="btn btn-primary" @click="increment">count is {{ count }}</button>
-  </div>
+    <div>
+        <button type="button" class="btn btn-primary" @click="increment">
+            count is {{ count }}
+        </button>
+    </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'pinia';
+import axios from 'axios';
 import counterStore from '../stores/counters';
 
 export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-    };
-  },
-  methods: {
-    ...mapActions(counterStore, ['increment']),
-  },
-  computed: {
-    ...mapState(counterStore, ['count']),
-  },
+    name: 'HelloWorld',
+    data() {
+        return {
+            data: {},
+        };
+    },
+    computed: {
+        ...mapState(counterStore, ['count']),
+    },
+    mounted() {
+        console.log(import.meta.env.VITE_API_URL);
+        this.fetchData();
+    },
+    methods: {
+        ...mapActions(counterStore, ['increment']),
+        fetchData() {
+            axios
+                .get(`${import.meta.env.VITE_API_URL}`)
+                .then((response) => {
+                    this.data = response.data;
+                })
+                .catch((error) => {
+                    console.error('API error:', error);
+                });
+        },
+    },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
