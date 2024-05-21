f9f282041afb6fdc81cd287d1632d1f8882ae085
hyxfish27 <80030772+hyxfish27@users.noreply.github.com>
2024-05-21 20:26:01 +0800

    feat: 登入註冊頁面切版 & 驗證 (#16)
    feat: 新增基本路由和樣式 (登入、註冊、修改密碼，Bootstrap 客製化樣式)
    feat: 環境配置和開發工具設定 (#4, #6, #15)
    -配置環境變數
    -配置 ESLint 和 Prettier
    -配置 Vite 開發伺服器和 HMR
    feat: 增加和調整功能頁面 (選擇角色頁, 玩家/店家登入頁, 店家/玩家資料頁編輯功能)
    feat: 修正 ESLint 錯誤、API 流程畫面、代碼排版，移除確認資料頁面，增加表單套件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,40 @@
 <template>
-  <div>
-    <button type="button" class="btn btn-primary" @click="increment">count is {{ count }}</button>
-  </div>
+    <div>
+        <button type="button" class="btn btn-primary" @click="increment">
+            count is {{ count }}
+        </button>
+    </div>
 </template>
-
 <script>
 import { mapActions, mapState } from 'pinia';
-import counterStore from '../stores/counters';
+import Axios from '@/utilities/axios';
+import counterStore from '@/stores/counters';
 
 export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-    };
-  },
-  methods: {
-    ...mapActions(counterStore, ['increment']),
-  },
-  computed: {
-    ...mapState(counterStore, ['count']),
-  },
+    name: 'HelloWorld',
+    data() {
+        return {
+            data: {},
+        };
+    },
+    computed: {
+        ...mapState(counterStore, ['count']),
+    },
+    mounted() {
+        this.fetchData();
+    },
+    methods: {
+        ...mapActions(counterStore, ['increment']),
+        fetchData() {
+            Axios.get('/')
+                .then((response) => {
+                    this.data = response.data;
+                })
+                .catch((error) => {
+                    console.error('API error:', error);
+                });
+        },
+    },
 };
 </script>
-
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/views/player/Login.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container player-login">
+        <div class="row align-content-center vh-100 justify-content-center">
+            <div class="col-5 align-self-center justify-content-center">
+                <h3 class="text-center">我是玩家</h3>
+            </div>
+            <div class="col-7 align-self-center justify-content-center">
+                <h3 class="text-center mb-4">玩家登入</h3>
+                <form>
+                    <div class="form-group mb-3">
+                        <label for="account">帳號</label>
+                        <input
+                            id="account"
+                            type="email"
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder="請輸入帳號"
+                        />
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="password"> 密碼 </label>
+                        <input
+                            id="password"
+                            type="password"
+                            class="form-control"
+                            placeholder="請輸入密碼"
+                        />
+                        <div
+                            class="password-forget d-flex justify-content-end mt-1"
+                            @click="goToForgetPasswordPage"
+                        >
+                            <span class="text-muted text-decoration-underline"
+                                >忘記密碼</span
+                            >
+                        </div>
+                    </div>
+
+                    <div class="d-flex justify-content-center form-footer">
+                        <button type="submit" class="btn btn-primary">
+                            登入
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerLogin',
+    data() {
+        return {};
+    },
+    methods: {
+        goToForgetPasswordPage() {
+            this.$router.push({
+                name: 'PasswordForget',
+                params: { role: 'player' },
+            });
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
