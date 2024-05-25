6aecffd6f943b4c2986ee40e64a0f9fc040d70d2
hyxfish27 <avicky1515@gmail.com>
2024-05-25 13:56:06 +0800

    feat: 全站共用 layout (header, footer)

---
+++ src/components/common/PreLoginHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header class="prelogin-header">
+        <div class="row">
+            <div class="col-3 header__logo">
+                <h3>聚人</h3>
+                <h6>Attack On Game</h6>
+            </div>
+            <div class="col-6 d-flex align-items-center">
+                <ul class="menu-list">
+                    <li class="menu-list__item">
+                        <router-link>活動列表</router-link>
+                    </li>
+                    <li>這是未登頁面</li>
+                </ul>
+            </div>
+            <div
+                class="col-3 header__nav d-flex justify-content-end align-items-center"
+            >
+                <router-link to="/login">登入</router-link>
+                <router-link to="/register">註冊</router-link>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+/**
+ * PreLoginHeader
+ * @author Vicky
+ * @description 未登頁面共用 Header
+ */
+export default {
+    name: 'PreLoginHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.prelogin-header {
+    .menu-list {
+        display: flex;
+        justify-content: start;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+}
+</style>
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="d-flex flex-column min-vh-100">
+        <MainHeader :is-login="isLogin"></MainHeader>
+        <router-view />
+        <MainFooter class="mt-auto"></MainFooter>
+    </div>
+</template>
+
+<script>
+import MainHeader from '@/components/common/MainHeader.vue';
+import MainFooter from '@/components/common/MainFooter.vue';
+
+/**
+ * Layout
+ * @author Vicky
+ * @description 全站共用 Layout
+ */
+export default {
+    name: 'Layout',
+    components: {
+        MainHeader,
+        MainFooter,
+    },
+    data() {
+        return {
+            isLogin: true,
+        };
+    },
+};
+</script>
