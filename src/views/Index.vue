13a7f34827479673ba2f9c76e190393d69cdc81d
Pei Ying <eee317@gmail.com>
2024-06-02 11:57:01 +0800

    註解不會用到的程式碼

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,26 +8,46 @@
         <br />
 
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+        >
             Launch demo modal
         </button>
 
         <!-- Modal -->
-        <div id="exampleModal" class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div
+            id="exampleModal"
+            class="modal fade"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 id="exampleModalLabel" class="modal-title fs-5">
                             Modal title
                         </h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
                     </div>
                     <div class="modal-body">
                         Lorem ipsum dolor sit amet consectetur, adipisicing
                         elit. Adipisci, consequatur.
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
                             Close
                         </button>
                         <button type="button" class="btn btn-primary">
@@ -51,24 +71,24 @@ import HelloWorld from '../components/HelloWorld.vue';
 // const theUserData = user();
 // console.log(theUserData.userData);
 
-const getPlayer = async (userId) => {
-    await PlayerAPI.get(userId)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch(() => {
-            router.push({
-                name: 'PlayerForm',
-            });
-        });
-};
-onMounted(() => {
-    const { isLogin } = theUserData;
-    if (isLogin) {
-        const userId = theUserData.userData._id;
-        getPlayer(userId);
-    }
-});
+// const getPlayer = async (userId) => {
+//     await PlayerAPI.get(userId)
+//         .then((res) => {
+//             console.log(res);
+//         })
+//         .catch(() => {
+//             router.push({
+//                 name: 'PlayerForm',
+//             });
+//         });
+// };
+// onMounted(() => {
+//     const { isLogin } = theUserData;
+//     if (isLogin) {
+//         const userId = theUserData.userData._id;
+//         getPlayer(userId);
+//     }
+// });
 </script>
 
 <style lang="scss" scoped></style>
