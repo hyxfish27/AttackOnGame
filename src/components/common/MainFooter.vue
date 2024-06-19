dc315320550ead9d3fb27df35981d2346f37d4d1
personal <k01midy@gmail.com>
2024-06-20 00:23:40 +0800

    feat:create-footer

--- src/components/common/MainFooter.vue
+++ src/components/common/MainFooter.vue
@@ -1,30 +1,40 @@
 <template>
-    <footer class="main-footer container-fluid">
-        <div>
-            <div class="col-3">
-                <h3>聚人</h3>
-                <h6>Attack On Game</h6>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-end">
+                <div class="img-wrap">
+                    <img class="w-100" :src="Logo" alt="" />
+                </div>
+                <div class="text">
+                    <p>本網站不提供商業用途</p>
+                    <p>copyright © 2024 聚人. All rights reserved.</p>
+                </div>
             </div>
         </div>
     </footer>
 </template>
 
-<script>
+<script setup>
 /**
  * MainFooter
  * @author vicky
  * @description 全站共用 MainFooter
  */
-export default {
-    name: 'MainFooter',
-};
+import Logo from '@/assets/images/img_logo.png';
 </script>
 
 <style lang="scss" scoped>
 .main-footer {
-    min-height: 100px;
-    background-color: #180000;
-    padding: 12px 0;
-    bottom: 0;
+    min-height: 128px;
+    padding: 32px 0;
+    background-color: #333333;
+    .img-wrap {
+        width: 240px;
+        height: 128px;
+    }
+    .text {
+        color: #d4d4d4;
+        font-size: 1rem;
+    }
 }
 </style>
