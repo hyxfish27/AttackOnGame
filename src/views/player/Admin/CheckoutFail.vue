9cadc2f87d5c1f1e22aa81a057b899def7104768
Pei Ying <eee317@gmail.com>
2024-06-19 20:54:20 +0800

    頁面調整

--- src/views/player/Admin/CheckoutFail.vue
+++ src/views/player/Admin/CheckoutFail.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">結帳失敗</div>
+    <div class="container">
+        <div class="row">
+            <div class="col">結帳失敗</div>
+        </div>
     </div>
-  </div>
 </template>
--- src/views/player/Admin/CheckoutSuccess.vue
+++ src/views/player/Admin/CheckoutSuccess.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">結帳成功</div>
+    <div class="container">
+        <div class="row">
+            <div class="col">結帳成功</div>
+        </div>
     </div>
-  </div>
 </template>
