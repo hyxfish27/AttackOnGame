6d6554ef6fe50b524a8429c2457ee427550f08cf
Benson <eagle163013@gmail.com>
2024-06-21 22:40:40 +0800

    feat: add ActivityTable component for displaying activity details

---
+++ src/components/player/ActivityTable.vue
@@ -0,0 +1,97 @@
+<script setup>
+// import { defineProps } from 'vue';
+
+// const props = defineProps({
+//     activityList: {
+//         type: Array,
+//         default: [],
+//     },
+// });
+
+const tableTitles = ['活動名稱', '', '時間', '金額', '張數', '訂單編號'];
+</script>
+
+<template>
+    <div class="d-flex flex-column justify-content-between h-100">
+        <!-- table -->
+        <table class="table align-middle table-hover mt-3">
+            <thead>
+                <tr>
+                    <th
+                        v-for="(title, index) in tableTitles"
+                        :key="index"
+                        scope="col"
+                    >
+                        {{ title }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="d-flex flex-column align-items-start">
+                            <p class="h5 flex-grow-1">活動名稱</p>
+                            <!-- 時間 -->
+                            <div class="d-flex">
+                                <p>2021-09-01</p>
+                                <p>19:00~22:00</p>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="d-flex flex-column">
+                            <button
+                                type="button"
+                                class="btn btn-outline-dark btn-sm mb-2"
+                            >
+                                前往票卷
+                            </button>
+                            <button
+                                v-if="true"
+                                type="button"
+                                class="btn btn-outline-dark btn-sm"
+                            >
+                                前往評價
+                            </button>
+                        </div>
+                    </td>
+                    <td>19:00</td>
+                    <td>$200</td>
+                    <td>1張</td>
+                    <td>O-240612-A3Q4</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- pafination -->
+        <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-end mt-3">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+.my-component {
+    /* 在這裡為你的組件添加樣式 */
+    padding: 20px;
+    background-color: #f0f0f0;
+}
+</style>
--- src/views/player/admin/Activity.vue
+++ src/views/player/admin/Activity.vue
@@ -1,15 +1,18 @@
-<script setup></script>
+<script setup>
+// import { ref } from 'vue';
+import ActivityTable from '@/components/player/ActivityTable.vue';
+</script>
 
 <template>
-    <main class="container pt-4 bg-light">
-        <div class="row gx-3">
+    <main class="container py-4 d-flex flex-column flex-grow-1">
+        <div class="row gx-2 flex-grow-1">
             <div class="col-4">
-                <div class="p-3 border rounded">Custom column padding</div>
+                <div class="p-3 border">Custom column padding</div>
             </div>
-            <div class="col-8 p-3 border rounded">
-                <div class="d-flex flex-column align-self-end">
-                    <div>
-                        <!-- tabli title -->
+            <div class="col-8">
+                <div class="p-3 border h-100">
+                    <div class="flex-column">
+                        <!-- table title -->
                         <div class="border-bottom py-2 mb-2">
                             <h7>活動管理</h7>
                         </div>
@@ -21,14 +24,12 @@
                             >
                                 即將到來
                             </button>
-
                             <button
                                 type="button"
                                 class="btn btn-outline-primary ms-2"
                             >
                                 已完成
                             </button>
-
                             <button
                                 type="button"
                                 class="btn btn-outline-primary ms-2"
@@ -36,63 +37,8 @@
                                 取消
                             </button>
                         </div>
-                        <!-- table -->
-                        <table class="table table-hover mt-3">
-                            <thead>
-                                <tr>
-                                    <th scope="col">活動名稱</th>
-                                    <th scope="col">活動日期</th>
-                                    <th scope="col">活動地點</th>
-                                    <th scope="col">活動狀態</th>
-                                    <th scope="col">操作</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>活動名稱</td>
-                                    <td>活動日期</td>
-                                    <td>活動地點</td>
-                                    <td>活動狀態</td>
-                                    <td>
-                                        <div class="d-flex flex-column">
-                                            <button
-                                                type="button"
-                                                class="btn btn-outline-primary mb-2"
-                                            >
-                                                編輯
-                                            </button>
-                                            <button
-                                                type="button"
-                                                class="btn btn-outline-primary"
-                                            >
-                                                刪除
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div>
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-end">
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Previous</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">1</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">2</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">3</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Next</a>
-                                </li>
-                            </ul>
-                        </nav>
+
+                        <ActivityTable />
                     </div>
                 </div>
             </div>
