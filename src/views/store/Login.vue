a42b0778eb0db214f3f53cfa35ba6b9f994e2f39
Pei Ying <38075054+eee317@users.noreply.github.com>
2024-06-22 00:07:02 +0800

    feat: 店家登入 (#58)
    * 登入表單元件化
    
    * 註解登入需符合英文的條件
    
    * 店家、玩家引入登入元件
    
    * 修正忘記密碼存取的角色值

--- src/views/store/Login.vue
+++ src/views/store/Login.vue
@@ -6,60 +6,22 @@
             </div>
             <div class="col-7 align-self-center justify-content-center">
                 <h3 class="text-center mb-4">店家登入</h3>
-                <form>
-                    <div class="form-group mb-3">
-                        <label for="account">帳號</label>
-                        <input
-                            id="account"
-                            type="email"
-                            class="form-control"
-                            aria-describedby="emailHelp"
-                            placeholder="請輸入帳號"
-                        />
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="password"> 密碼 </label>
-                        <input
-                            id="password"
-                            type="password"
-                            class="form-control"
-                            placeholder="請輸入密碼"
-                        />
-                        <div
-                            class="password-forget d-flex justify-content-end mt-1"
-                            @click="goToForgetPasswordPage"
-                        >
-                            <span class="text-muted text-decoration-underline"
-                                >忘記密碼</span
-                            >
-                        </div>
-                    </div>
-
-                    <div class="d-flex justify-content-center form-footer">
-                        <button type="submit" class="btn btn-primary">
-                            登入
-                        </button>
-                    </div>
-                </form>
+                <loginForm></loginForm>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import loginForm from '@/components/common/Login.vue';
+
 export default {
     name: 'StoreLogin',
+    components: {
+        loginForm,
+    },
     data() {
         return {};
-    },
-    methods: {
-        // TODO: 字串 'store' 要用 enum 存起來
-        goToForgetPasswordPage() {
-            this.$router.push({
-                name: 'PasswordForget',
-                params: { role: 'store' },
-            });
-        },
     },
 };
 </script>
