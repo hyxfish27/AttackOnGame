95efb271152d28090398bc14f576caa75ab469ca
Pei Ying <38075054+eee317@users.noreply.github.com>
2024-06-22 15:26:35 +0800

    feat: 修正結帳頁、確認結帳頁來回切換 value 消失的問題 (#74)
    * 完成結帳頁基本切版
    
    * 結帳成功、結帳失敗頁面切版完成
    
    * BS5 modal 調整
    
    * 補金額
    
    * 新增確認付款頁面
    
    * 串接 getSummary API
    
    * 完成 API 串接、reCheckout載入對應資料
    
    * 修正 API 串接、Pinia的資料傳遞
    
    * 完成票券切版
    
    * 安裝套件 pinia-plugin-persistedstate
    
    * 修正來回切換頁面value消失的問題

--- src/stores/order.js
+++ src/stores/order.js
@@ -13,14 +13,35 @@ export const useFormStore = defineStore('form', {
             note: '',
             payPrice: 0,
         },
+        isNew: true,
     }),
     actions: {
         updateFormData(data) {
             this.formData = data;
         },
+        setState(state) {
+            this.isNew = state;
+            if (this.isNew === true) {
+                this.formData = {
+                    title: '',
+                    address: '',
+                    startTime: '',
+                    endTime: '',
+                    userName: '',
+                    phoneNum: '',
+                    personNum: 1,
+                    note: '',
+                    payPrice: 0,
+                };
+            }
+        },
     },
     getters: {
         getFormData: (state) => state.formData,
+        getFormState: (state) => state.isNew,
+    },
+    persist: {
+        key: 'player-order-form-data',
     },
 });
 export default useFormStore;
