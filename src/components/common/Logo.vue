582fe235636097b1ba073bb22dbf0d3bebc2cae6
mesh <kami.yh@medialand.tw>
2024-06-22 10:40:58 +0800

    feat:[issue45]首頁切版

---
+++ src/components/common/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-link class="col-3 header__logo" to="/">
+        <div class="img-wrap h-100">
+            <img class="h-100" :src="Logo" alt="" />
+        </div>
+    </router-link>
+</template>
+<script setup>
+import Logo from '@/assets/images/img_logo.png';
+</script>
+<style>
+.header__logo {
+    height: 84px;
+}
+</style>
