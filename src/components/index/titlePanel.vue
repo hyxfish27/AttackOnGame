c49a8c7d78b97d3512017f090a26dc3afb1988f6
Pei Ying <eee317@gmail.com>
2024-06-22 11:36:26 +0800

    調整 titlePanel

--- src/components/index/titlePanel.vue
+++ src/components/index/titlePanel.vue
@@ -3,14 +3,14 @@
         class="index-title-wrap d-flex flex-column align-items-start position-relative"
     >
         <div class="tag">
-            <p class="manuale">{{ tag }}</p>
+            <p class="manuale text-white">{{ tag }}</p>
         </div>
         <div class="title-wrap">
             <div class="bg-yellow title d-inline-block position-relative">
-                <h2 class="mb-0">{{ title }}</h2>
+                <h2 class="mb-0 noto-serif-tc">{{ title }}</h2>
             </div>
             <div class="bg-greyE9 desc">
-                <h3 class="mb-0"># {{ desc }}</h3>
+                <h3 class="mb-0 noto-serif-tc"># {{ desc }}</h3>
             </div>
         </div>
     </div>
@@ -37,12 +37,17 @@ defineProps({
 <style lang="scss">
 .index-title-wrap {
     .tag {
+        font-weight: 800;
         font-size: 96px;
-        color: #000;
+
+        -webkit-text-stroke: 2px #000000;
+        text-stroke: 2px #000000;
     }
+
     .title {
         padding: 4px 64px 4px 12px;
     }
+
     .title::after {
         position: absolute;
         content: '/';
@@ -51,16 +56,20 @@ defineProps({
         top: 50%;
         transform: translateY(-50%);
     }
+
     .title-wrap {
         margin-top: -48px;
     }
+
     .desc {
         padding: 8px 12px;
     }
+
     h2 {
         font-size: 3rem;
         font-weight: 700;
     }
+
     h3 {
         font-weight: 700;
         font-size: 1.25rem;
