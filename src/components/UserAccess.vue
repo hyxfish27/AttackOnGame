a3a03c3e3ab37f8c51921c790bf376a59ffe26ff
Pei Ying <eee317@gmail.com>
2024-06-23 13:40:12 +0800

    微調小細節

--- src/components/UserAccess.vue
+++ src/components/UserAccess.vue
@@ -6,18 +6,20 @@
                     class="user-access_section user-access_aside d-flex flex-column justify-content-center align-items-center"
                 >
                     <div
-                        class="img-wrap mb-2 d-flex justify-content-center align-items-center"
+                        class="img-wrap mb-4 d-flex justify-content-center align-items-center"
                         :class="`${roleAttr.key}-icon`"
                     >
                         <img :src="roleAttr.icon" alt="" />
                     </div>
-                    <h2 class="text-center fw-12">我是{{ roleAttr.text }}</h2>
+                    <h2 class="text-center fs-4 fw-bold">
+                        我是{{ roleAttr.text }}
+                    </h2>
                 </div>
                 <div
                     class="user-access_main user-access_section d-flex flex-column justify-content-center"
                 >
                     <h1
-                        class="fw-12 title d-flex align-items-center"
+                        class="fs-5 pb-6 fw-bold title d-flex align-items-center"
                         :class="roleAttr.key"
                     >
                         填寫{{ roleAttr.text }}資訊
@@ -60,24 +62,26 @@ const roleAttr = computed(() => {
 <style lang="scss" scoped>
 .user-access_wrap {
     padding: 48px 0;
+
     .user-access_panel {
         gap: 48px;
     }
+
     .user-access_aside {
         width: 30%;
     }
+
     .user-access_main {
         width: 70%;
     }
+
     .user-access_section {
         border-radius: 8px;
         padding: 48px;
         border: 1px solid #d4d4d4;
         background-color: #fff;
     }
-    .fw-12 {
-        font-size: 3rem;
-    }
+
     .title.player::before {
         content: '';
         display: block;
@@ -86,6 +90,7 @@ const roleAttr = computed(() => {
         background-color: #ffdd33;
         margin-right: 16px;
     }
+
     .title.store::before {
         content: '';
         display: block;
@@ -94,17 +99,21 @@ const roleAttr = computed(() => {
         background-color: #ff8025;
         margin-right: 16px;
     }
+
     .img-wrap {
         width: 160px;
         height: 160px;
         border-radius: 50%;
+
         img {
             width: 80px;
             height: 80px;
         }
+
         &.player-icon {
             border: 8px solid #ffdd33;
         }
+
         &.store-icon {
             border: 8px solid #ff8025;
         }
