4b046e49ffedc470f6135fd7eb16f695169c8a28
mesh <kami.yh@medialand.tw>
2024-06-23 13:08:57 +0800

    feat:修正登入註冊頁面

---
+++ src/components/UserAccess.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="full-sceen-with-footer bg-greyF7">
+        <div class="container h-100 user-access_wrap">
+            <div class="d-flex justify-content-between user-access_panel h-100">
+                <div
+                    class="user-access_section user-access_aside d-flex flex-column justify-content-center align-items-center"
+                >
+                    <div
+                        class="img-wrap mb-2 d-flex justify-content-center align-items-center"
+                        :class="`${roleAttr.key}-icon`"
+                    >
+                        <img :src="roleAttr.icon" alt="" />
+                    </div>
+                    <h2 class="text-center fw-12">我是{{ roleAttr.text }}</h2>
+                </div>
+                <div
+                    class="user-access_main user-access_section d-flex flex-column justify-content-center"
+                >
+                    <h1 class="fw-12">填寫{{ roleAttr.text }}資訊</h1>
+                    <slot name="form"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import storeIcon from '@/assets/images/store-icon.png';
+import userIcon from '@/assets/images/user-icon.png';
+
+const ROLE_MAP = {
+    store: {
+        key: 'store',
+        text: '店家',
+        icon: storeIcon,
+    },
+    player: {
+        key: 'player',
+        text: '玩家',
+        icon: userIcon,
+    },
+};
+const { role } = defineProps({
+    role: {
+        type: String,
+        default: 'store',
+    },
+});
+const roleAttr = computed(() => {
+    return ROLE_MAP[role];
+});
+</script>
+
+<style lang="scss" scoped>
+.user-access_wrap {
+    padding: 48px 0;
+    .user-access_panel {
+        gap: 48px;
+    }
+    .user-access_aside {
+        width: 30%;
+    }
+    .user-access_main {
+        width: 70%;
+    }
+    .user-access_section {
+        border-radius: 8px;
+        padding: 48px;
+        border: 1px solid #d4d4d4;
+        background-color: #fff;
+    }
+    .fw-12 {
+        font-size: 3rem;
+    }
+    .img-wrap {
+        width: 160px;
+        height: 160px;
+        border-radius: 50%;
+        img {
+            width: 80px;
+            height: 80px;
+        }
+        &.player-icon {
+            border: 8px solid #ffdd33;
+        }
+        &.store-icon {
+            border: 8px solid #ff8025;
+        }
+    }
+}
+</style>
--- src/views/store/Login.vue
+++ src/views/store/Login.vue
@@ -1,29 +1,14 @@
 <template>
-    <div class="container store-login">
-        <div class="row align-content-center vh-100 justify-content-center">
-            <div class="col-5 align-self-center justify-content-center">
-                <h3 class="text-center">我是店家</h3>
-            </div>
-            <div class="col-7 align-self-center justify-content-center">
-                <h3 class="text-center mb-4">店家登入</h3>
-                <loginForm></loginForm>
-            </div>
-        </div>
-    </div>
+    <UserAccess role="store">
+        <template #form>
+            <loginForm></loginForm>
+        </template>
+    </UserAccess>
 </template>
 
-<script>
+<script setup>
+import UserAccess from '@/components/UserAccess.vue';
 import loginForm from '@/components/common/Login.vue';
-
-export default {
-    name: 'StoreLogin',
-    components: {
-        loginForm,
-    },
-    data() {
-        return {};
-    },
-};
 </script>
 
 <style lang="scss" scoped>
