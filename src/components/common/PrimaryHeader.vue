ec6fa5452fb130a8a0fb5d7c29b0c53706bfef5f
ML-Kami <125852208+ML-Kami@users.noreply.github.com>
2024-06-23 15:42:45 +0800

    feat-更新header上Logo無法顯示問題，並抽取共用元件 (#79)

---
+++ src/components/common/PrimaryHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="d-flex align-items-center">
+        <Logo></Logo>
+        <div>
+            <ul class="menu-list d-flex align-items-center">
+                <li class="menu-list__item">
+                    <router-link to="/event-list">活動列表 </router-link>
+                </li>
+                <li class="menu-list__item">
+                    <router-link to="/shop-list"> 店家列表 </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script setup>
+import Logo from '@/components/common/Logo.vue';
+</script>
+<style lang="scss" scoped>
+.menu-list {
+    display: flex;
+    justify-content: start;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.menu-list__item + .menu-list__item {
+    margin-left: 16px;
+}
+// TODO: router-link style 需要從變數檔控制
+</style>
