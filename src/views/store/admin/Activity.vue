a781d1ebfefeef9c4bad4381857a6ea79b0a08e1
Pei Ying <38075054+eee317@users.noreply.github.com>
2024-06-23 23:59:26 +0800

    feat: 店家活動列表切版、店家後台共用元件 (#100)
    * 加入共用側欄元件，完成活動列表切版
    
    * build 整理

--- src/views/store/admin/Activity.vue
+++ src/views/store/admin/Activity.vue
@@ -1,3 +1,12 @@
 <template>
+  <div class="container">
+    <div class="row p-3">
+      <LeftEl></LeftEl>
+      <div class="col-9 border rounded bg-white h-100 p-3"></div>
+    </div>
+  </div>
   <div>店家活動詳細</div>
 </template>
+<script setup>
+import LeftEl from '@/components/store/StoreLeftEl.vue';
+</script>
