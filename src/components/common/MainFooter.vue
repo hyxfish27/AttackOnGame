d123185af45d845858eb8e32195065ca9b91f1e7
mesh <kami.yh@medialand.tw>
2024-06-23 10:17:26 +0800

    feat(index):修正首頁圖片無法滿版的問題

--- src/components/common/MainFooter.vue
+++ src/components/common/MainFooter.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="d-flex justify-content-between align-items-end">
                 <div class="img-wrap">
-                    <img class="w-100" :src="Logo" alt="" />
+                    <img class="h-100" :src="Logo" alt="" />
                 </div>
                 <div class="text text-end noto-serif-tc">
                     <p>本網站不提供商業用途</p>
@@ -25,12 +25,10 @@ import Logo from '@/assets/images/img_logo.png';
 
 <style lang="scss" scoped>
 .main-footer {
-    min-height: 128px;
-    padding: 32px 0;
+    padding: 8px 0;
     background-color: #333333;
     .img-wrap {
-        width: 240px;
-        height: 128px;
+        height: 84px;
     }
     .text {
         color: #d4d4d4;
