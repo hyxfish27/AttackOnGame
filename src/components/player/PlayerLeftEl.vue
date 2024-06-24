081f04997cba10beb8f246028226a6a7b6dd4d45
ML-Kami <125852208+ML-Kami@users.noreply.github.com>
2024-06-24 10:42:06 +0800

    feat(player):表單頁面的版型與功能調整 (#102)
    * feat-player-activity
    
    * feat:修改付款狀態為中文
    
    * 調整樣式、修正螢幕 100%
    破版的問題
    
    ---------
    
    Co-authored-by: Pei Ying <eee317@gmail.com>

--- src/components/player/PlayerLeftEl.vue
+++ src/components/player/PlayerLeftEl.vue
@@ -1,22 +1,26 @@
 <template>
-    <div
-        class="col-3 p-3 border rounded bg-white h-100"
-        style="min-height: 80vh"
-    >
+    <div class="p-3 border rounded bg-white">
         <div>
             <p class="fs-8 fw-bold text-grey66">玩家帳戶資訊</p>
             <!-- <img class="player-admin__img" :url="formData.url" /> -->
             <div
+                v-if="isImg"
                 style="width: 130px; height: 130px"
-                class="border-5 border border-warning rounded-circle p-4 mx-auto mt-5 mb-2"
+                class="img-wrap border-5 border border-warning rounded-circle mx-auto mt-5 mb-2"
             >
                 <img
-                    class="w-100"
-                    src="/src/assets/images/user-icon.png"
+                    class="avatar"
+                    :src="indexStore.playerData.avatar"
                     alt="玩家圖片"
                 />
             </div>
-
+            <div
+                v-else
+                style="width: 130px; height: 130px"
+                class="border-5 border border-warning rounded-circle p-4 mx-auto mt-5 mb-2"
+            >
+                <img class="w-100" :src="defaultPic" alt="玩家圖片" />
+            </div>
             <!-- <div>{{ formData.email }}</div> -->
             <div class="mb-4 text-center fw-bold">
                 {{ indexStore.playerData.name }}
@@ -25,7 +29,7 @@
             <div class="d-flex flex-column gap-2">
                 <button
                     type="button"
-                    class="p-2 btn w-100"
+                    class="p-2 btn w-100 side-list"
                     @click="goPlayerPage"
                 >
                     我的帳戶
@@ -33,7 +37,7 @@
                 <!-- <div>平台幣</div> -->
                 <button
                     type="button"
-                    class="p-2 btn w-100 m-0"
+                    class="p-2 btn w-100 m-0 side-list"
                     @click="goActivityPage"
                 >
                     我的活動
@@ -43,8 +47,10 @@
     </div>
 </template>
 <script setup>
+import { computed } from 'vue';
 import { useRouter } from 'vue-router';
 import useIndexStore from '@/stores/index';
+import defaultPic from '@/assets/images/user-icon.png';
 
 const router = useRouter();
 const indexStore = useIndexStore();
@@ -60,4 +66,21 @@ const goActivityPage = () => {
         name: 'PlayerActivity',
     });
 };
+
+const isImg = computed(() => {
+    return !!indexStore.playerData.avatar;
+});
 </script>
+<style lang="scss" scoped>
+.avatar {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.img-wrap {
+    overflow: hidden;
+}
+.side-list:hover {
+    background-color: #eeecec;
+}
+</style>
