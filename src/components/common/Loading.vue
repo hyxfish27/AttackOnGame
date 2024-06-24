4a30493302d9c4f7d9f6dd8ebf631728195fbfb1
ML-Kami <125852208+ML-Kami@users.noreply.github.com>
2024-06-24 17:32:01 +0800

    feat(single-event):修正商品明細頁面細節 (#106)
    * save
    
    * feat(single-event):修正商品明細頁面細節
    
    * feat:更新資料格式
    
    * feat:修正圖片變形的問題
    
    * feat(router):修正店家頁面連結
    
    * feat:新增轉圈loading
    
    * 調整載入中狀態
    
    ---------
    
    Co-authored-by: Pei Ying <eee317@gmail.com>

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="loding-area">
+        <div class="text-primary d-flex align-items-center">
+            <div
+                class="py-3 px-4 bg-white shadow border h-30 d-flex align-items-center rounded-2"
+            >
+                <div class="loader-search"></div>
+                <p class="flex-grow-1 text fw-bold">資料搜尋中...</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+.text {
+    margin-left: 20px;
+}
+
+.loding-area {
+    left: 0;
+    display: flex;
+    position: fixed;
+    justify-content: center;
+    right: 0;
+    height: 80vh;
+    transition: opacity 0.5s ease;
+}
+
+.fadeout {
+    opacity: 0;
+}
+</style>
