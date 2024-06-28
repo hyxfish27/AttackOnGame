940ab1c2d75a4451f1b7a0379a9b818870515056
mesh <kami.yh@medialand.tw>
2024-06-28 19:45:26 +0800

    feat-優化活動詳細頁的icon設計與路由設計

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,5 @@
 <template class="">
-    <div class="bg-greyF7 event-list-wrap">
+    <div class="bg-greyF7 min-full-screen-with-footer">
         <div class="container pb-5">
             <div class="row py-7 justify-content-center">
                 <div
@@ -154,8 +154,4 @@ onMounted(() => {
     getEvent(query.value);
 });
 </script>
-<style>
-.event-list-wrap {
-    min-height: calc(100vh - 312px);
-}
-</style>
+<style></style>
