9947af69243c1288dfdbc1579fb93e88040e45aa
mesh <kami.yh@medialand.tw>
2024-06-29 00:39:41 +0800

    feat-修正表格中無資料時的前端畫面

---
+++ src/components/common/EmptyTable.vue
@@ -0,0 +1,24 @@
+<template>
+    <tbody>
+        <tr>
+            <td :colspan="length">
+                <div
+                    class="d-flex justify-content-center text-grey9F mt-3 mb-3"
+                >
+                    <span class="material-symbols-outlined">
+                        sentiment_dissatisfied
+                    </span>
+                    <p>{{ text }}</p>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    length: { type: Number, default: 1 },
+    text: { type: String, default: '目前還沒有蒐錄到資料唷' },
+});
+</script>
