66d0b26de517166be478f8c12e133a04e6c9518e
Pei Ying <eee317@gmail.com>
2024-06-30 14:53:06 +0800

    微調無資料狀態樣式

--- src/components/common/EmptyTable.vue
+++ src/components/common/EmptyTable.vue
@@ -3,12 +3,12 @@
         <tr>
             <td :colspan="length">
                 <div
-                    class="d-flex justify-content-center text-grey9F mt-3 mb-3"
+                    class="d-flex justify-content-center text-grey9F mt-3 mb-3 align-items-center"
                 >
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined p-2">
                         sentiment_dissatisfied
                     </span>
-                    <p>{{ text }}</p>
+                    <p class="fw-bold">{{ text }}</p>
                 </div>
             </td>
         </tr>
