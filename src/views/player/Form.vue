2bd9976f2ce760264bac04e34c1ae354dbbec49e
Pei Ying <eee317@gmail.com>
2024-07-06 22:06:26 +0800

    玩家建立表單，可上傳圖片

--- src/views/player/Form.vue
+++ src/views/player/Form.vue
@@ -5,73 +5,43 @@
                 <v-form v-slot="{ errors }">
                     <div class="mb-3">
                         <label for="playerName" class="form-label">暱稱</label>
-                        <v-field
-                            id="playerName"
-                            v-model="userForm.name"
-                            type="text"
-                            class="form-control"
-                            aria-describedby="playerName"
-                            name="暱稱"
-                            rules="required"
-                            :class="{ 'is-invalid': errors['暱稱'] }"
-                        ></v-field>
-                        <error-message
-                            name="暱稱"
-                            class="text-danger"
-                        ></error-message>
+                        <v-field id="playerName" v-model="userForm.name" type="text" class="form-control"
+                            aria-describedby="playerName" name="暱稱" rules="required"
+                            :class="{ 'is-invalid': errors['暱稱'] }"></v-field>
+                        <error-message name="暱稱" class="text-danger"></error-message>
                     </div>
                     <div class="mb-3">
                         <label for="playerPhone" class="form-label">手機</label>
-                        <v-field
-                            id="playerPhone"
-                            v-model="userForm.phone"
-                            type="text"
-                            class="form-control"
-                            aria-describedby="playerPhone"
-                            name="手機"
-                            rules="required|min:10|max:10"
-                            :class="{ 'is-invalid': errors['手機'] }"
-                        >
+                        <v-field id="playerPhone" v-model="userForm.phone" type="text" class="form-control"
+                            aria-describedby="playerPhone" name="手機" rules="required|min:10|max:10"
+                            :class="{ 'is-invalid': errors['手機'] }">
                         </v-field>
-                        <error-message
-                            name="手機"
-                            class="text-danger"
-                        ></error-message>
+                        <error-message name="手機" class="text-danger"></error-message>
+                    </div>
+                    <div class="mb-3">
+                        <label for="avatar" class="form-label">上傳頭像</label>
+                        <input id="avatar" type="file" class="form-control" name="avatar" @change="handleFileUpload" />
+                        <error-message name="avatar" class="text-danger"></error-message>
                     </div>
                     <div class="mb-3">
                         <h3 class="fs-6 fw-bold mt-3">喜歡的桌遊類型</h3>
                         <div class="form-check">
-                            <input
-                                id="favorite"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="角色扮演遊戲(RPG)"
-                            />
+                            <input id="favorite" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="角色扮演遊戲(RPG)" />
                             <label class="form-check-label" for="favorite">
                                 角色扮演遊戲(RPG)
                             </label>
                         </div>
                         <div class="form-check">
-                            <input
-                                id="favorite2"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="派對遊戲"
-                            />
+                            <input id="favorite2" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="派對遊戲" />
                             <label class="form-check-label" for="favorite2">
                                 派對遊戲
                             </label>
                         </div>
                         <div class="form-check">
-                            <input
-                                id="favorite3"
-                                v-model="userForm.preferGame"
-                                class="form-check-input"
-                                type="checkbox"
-                                value="策略遊戲"
-                            />
+                            <input id="favorite3" v-model="userForm.preferGame" class="form-check-input" type="checkbox"
+                                value="策略遊戲" />
                             <label class="form-check-label" for="favorite3">
                                 策略遊戲
                             </label>
@@ -89,18 +59,38 @@
 <script setup>
 import UserAccess from '@/components/UserAccess.vue';
 import PlayerAPI from '@/api/Player';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
+import useIndexStore from '@/stores/index';
+import ImageAPI from '@/api/Image';
 
+const indexStore = useIndexStore();
+const userData = computed(() => indexStore.userData);
 const router = useRouter();
 const userForm = ref({
     name: '',
     phone: '',
-    avatar: 'https://example.com/avatar.jpg',
+    avatar: '',
     preferGame: [],
 });
-// const theUserData = user();
 
+const postImage = async (userId, file) => {
+    await ImageAPI.postPlayerImg(userId, file)
+        .then((res) => {
+            userForm.value.avatar = res.data.imgURL;
+            console.log('res.imgURL', res.data.imgURL);
+            // window.location.reload();
+        })
+        .catch((err) => {
+            console.log('imageErr', err);
+        });
+};
+
+const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    userForm.value.avatar = file;
+    postImage(userData.value.id, userForm.value.avatar);
+};
 const postUserForm = async (userFormData) => {
     if (
         userForm.value.name === '' ||
